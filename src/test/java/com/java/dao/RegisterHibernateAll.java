c561721aca9ca9aec9d8b16b0e0b54682b768801
Hamit Mizrak <hamitmizrak@gmail.com>
2020-08-07 23:01:06 +0300

    EntityManagerFactor ve ManagerFactor metodu yazıldı

--- src/test/java/com/java/dao/RegisterHibernateAll.java
+++ src/test/java/com/java/dao/RegisterHibernateAll.java
@@ -1,7 +1,14 @@
 package com.java.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.java.context.HibernatePersistenceUnitKey;
+
 public class RegisterHibernateAll {
 /*
+ Paketler= javax.persistence üzerinden alıyoruz.
  
  Create-Delete-Update için mutlaka Transaction yapmak gerekiyor. ancak select için transcation yapmıyoruz.
  Create keyword=
@@ -11,4 +18,17 @@ public class RegisterHibernateAll {
  select keyword=
  
  */
+
+	private static EntityManagerFactory entityManagerFactory=null;
+	private static EntityManager entityManager=null;
+	private static HibernatePersistenceUnitKey hibernatePersistenceUnitKey;
+	
+	
+	//EntityManager Config
+	private static EntityManager getEntityManagerConfig(EntityManager entityManager) {
+		entityManagerFactory=Persistence.createEntityManagerFactory(hibernatePersistenceUnitKey.KEY);
+		entityManager=entityManagerFactory.createEntityManager();
+		return entityManager;
+	}
+	
 }
